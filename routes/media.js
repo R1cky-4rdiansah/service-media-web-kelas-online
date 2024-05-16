6a8e54a3b4a52c2b090cae2836a71b0951f9c42f
MazGeryM6 <99625033+MazGeryM6@users.noreply.github.com>
2024-05-16 15:06:18 +0700

    delete media

--- routes/media.js
+++ routes/media.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const isBase64 = require("is-base64");
 const base64img = require("base64-img");
 const { Media } = require("../models");
+const fs = require("fs");
 
 router.get("/", async (req, res) => {
   const media = await Media.findAll({ attributes: ["id", "image"] });
@@ -22,7 +23,7 @@ router.post("/", (req, res) => {
   const image = req.body.image;
 
   if (!isBase64(image, { mimeRequired: true })) {
-    return res.status(400).json({ status: "Error", message: "invalid base64" });
+    return res.status(400).json({ status: "error", message: "invalid base64" });
   }
 
   base64img.img(image, "./public/images", Date.now(), async (err, filepath) => {
@@ -39,6 +40,29 @@ router.post("/", (req, res) => {
         id: data.id,
         image: `${req.get("host")}/images/${filename}`,
       },
+    });
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const data = await Media.findByPk(id);
+
+  if (!data) {
+    return res.json({ status: 400, message: "Data tidak ditemukan" });
+  }
+
+  fs.unlink(`./public/${data.image}`, async (err) => {
+    if (err) {
+      return res.json({ status: 400, message: err.message });
+    }
+
+    await data.destroy();
+
+    return res.json({
+      status: "success",
+      message: "Data berhasil dihapus",
     });
   });
 });
